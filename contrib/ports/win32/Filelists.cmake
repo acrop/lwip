2ac2e1fd7ed8f19b68380b54e4be021bd19fd62c
Yonggang Luo <luoyonggang@gmail.com>
2020-06-06 13:54:27 +0800

    getting wpacp to be optional.

--- contrib/ports/win32/Filelists.cmake
+++ contrib/ports/win32/Filelists.cmake
@@ -15,8 +15,6 @@ endif()
 set(lwipcontribportwindows_SRCS
     ${LWIP_CONTRIB_DIR}/ports/win32/sys_arch.c
     ${LWIP_CONTRIB_DIR}/ports/win32/sio.c
-    ${LWIP_CONTRIB_DIR}/ports/win32/pcapif.c
-    ${LWIP_CONTRIB_DIR}/ports/win32/pcapif_helper.c
 )
 
 # pcapif needs WinPcap developer package: https://www.winpcap.org/devel.htm
@@ -26,9 +24,21 @@ if(NOT DEFINED WPDPACK_DIR)
 endif()
 find_library(WPCAP  wpcap  HINTS ${WPDPACK_DIR}/lib)
 find_library(PACKET packet HINTS ${WPDPACK_DIR}/lib)
+if(NOT WPCAP OR NOT PACKET)
+    message(STATUS "WPCAP: ${WPCAP} or PACKET:${PACKET}, disable pcap netif")
+else()
+    list(APPEND LWIP_DEFINITIONS
+        WPCAP_ENABLED=1
+    )
+    list(APPEND lwipcontribportwindows_SRCS
+        ${LWIP_CONTRIB_DIR}/ports/win32/pcapif.c
+        ${LWIP_CONTRIB_DIR}/ports/win32/pcapif_helper.c
+    )
+    list(APPEND LWIP_MBEDTLS_LINK_LIBRARIES ${WPCAP} ${PACKET})
+endif()
 
 add_library(lwipcontribportwindows EXCLUDE_FROM_ALL ${lwipcontribportwindows_SRCS})
 target_include_directories(lwipcontribportwindows PRIVATE ${LWIP_INCLUDE_DIRS} "${WPDPACK_DIR}/include" ${LWIP_MBEDTLS_INCLUDE_DIRS})
 target_compile_options(lwipcontribportwindows PRIVATE ${LWIP_COMPILER_FLAGS})
 target_compile_definitions(lwipcontribaddons PRIVATE ${LWIP_DEFINITIONS} ${LWIP_MBEDTLS_DEFINITIONS})
-target_link_libraries(lwipcontribportwindows PUBLIC ${WPCAP} ${PACKET} ${LWIP_MBEDTLS_LINK_LIBRARIES})
+target_link_libraries(lwipcontribportwindows PUBLIC ${LWIP_MBEDTLS_LINK_LIBRARIES})
